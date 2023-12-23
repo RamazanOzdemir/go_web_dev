ba740e400035eb5f86b18460f49758878b352ce7
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2023-12-24 00:53:43 +0100

    feat: templates

--- main.go
+++ main.go
@@ -5,7 +5,18 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
+	"html/template"
 )
+
+type Todo struct {
+	Title string
+	Done bool
+}
+
+type TodoPageData struct{
+	PageTitle string
+	TodoList []Todo
+}
 
 func main (){
 	r := mux.NewRouter()
@@ -13,6 +24,21 @@ func main (){
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r * http.Request){
 		fmt.Fprintf(w,"Welcome to my website" )
+	})
+
+	
+	todoTemplate := template.Must(template.ParseFiles("templates/todo.html"))
+	r.HandleFunc("/todo",  func(w http.ResponseWriter, r * http.Request){
+		data := TodoPageData{
+			PageTitle: "My Todo List",
+			TodoList: []Todo{
+				{Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+			},
+		}
+
+		todoTemplate.Execute(w, data)
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
