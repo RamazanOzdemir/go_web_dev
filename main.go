97796aed67ff8d0304eb17f6960d8eb653fb26cd
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2023-12-24 00:33:04 +0100

    feat: router

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
 
 func main (){
-	http.HandleFunc("/", func(w http.ResponseWriter, r * http.Request){
+	r := mux.NewRouter()
+
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r * http.Request){
 		fmt.Fprintf(w,"Welcome to my website" )
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
 
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 
-	http.ListenAndServe(":80", nil)
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+
+		title := vars["title"]
+		page := vars["page"]
+		        
+		
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+
+
+
+	})
+
+	
+	log.Println("Server will start at localhost")
+	http.ListenAndServe(":80", r)
 }
